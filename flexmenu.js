1fe4f6d6780af091689c5af77414dc899ca5c76b
Ryan Rife <rwrife@gmail.com>
2016-12-05 14:27:05 -0600

    removed samuel ipsum, fixed menu

--- flexmenu.js
+++ flexmenu.js
@@ -1,58 +1,59 @@
 var $fmFilterSpacer = $('<div />', {
-  "class": "filter-drop-spacer",
-  "height": $("#nav").outerHeight()
+    "class": "filter-drop-spacer",
+    "height": $("#nav").outerHeight()
 });
 
-$( window ).resize(function() {    
+$(window).resize(function() {
     _fmMenuRefresh();
 });
 
-$( document ).ready(function() {    
+$(document).ready(function() {
     _fmMenuRefresh();
 });
 
 
- $( window ).scroll(function(){    	   
-     if(!$("#nav").hasClass('fix') && $(window).scrollTop() > $("#nav").offset().top){
+$(window).scroll(function() {
+    if (!$("#nav").hasClass('fix') && $(window).scrollTop() > $("#nav").offset().top) {
         $("#nav").before($fmFilterSpacer);
-        $("#nav").addClass("fix");		  	
-     } else if ( $("#nav").hasClass('fix')  && $(window).scrollTop() < $fmFilterSpacer.offset().top){		 			 	
+        $("#nav").addClass("fix");
+    } else if ($("#nav").hasClass('fix') && $(window).scrollTop() < $fmFilterSpacer.offset().top) {
         $("#nav").removeClass("fix");
-        $filterSpacer.remove();
-     }
-     _fmMenuRefresh();
- });
+        if (typeof $filterSpacer !== 'undefined')
+            $filterSpacer.remove();
+    }
+    _fmMenuRefresh();
+});
 
 function _fmMenuRefresh() {
     var moreMenu = $("#nav > li.nav-more > ul").first();
     var menuBar = $("#nav").first();
-    
-    $("#nav > li.nav-more > ul > li").each(function(i,e) {
+
+    $("#nav > li.nav-more > ul > li").each(function(i, e) {
         var item = $(e);
-        if(item.outerWidth() < _fmMenuBarSpace()) {            
-            item.appendTo(menuBar);            
+        if (item.outerWidth() < _fmMenuBarSpace()) {
+            item.appendTo(menuBar);
         } else return false;
     });
-      
+
     $("#nav > li:not(.nav-more)").each(function(i, e) {
         var item = $(e);
         var pos = item.position();
-        if(pos != null && pos.top > 0) {                       
+        if (pos != null && pos.top > 0) {
             item.prependTo(moreMenu);
-        }        
+        }
     });
-    
-    if(moreMenu.children().length == 0) {
+
+    if (moreMenu.children().length == 0) {
         $("#nav > li.nav-more").hide();
     } else {
-         $("#nav > li.nav-more").show();
+        $("#nav > li.nav-more").show();
     }
 }
 
 function _fmMenuBarSpace() {
     var menuSpace = 0;
     var lastItem = $("#nav > li:not(.nav-more):last");
-    if(lastItem != null) {
+    if (lastItem != null) {
         menuSpace = $("#nav").innerWidth() - (lastItem.position().left + lastItem.outerWidth());
     } else {
         menuSpace = $("#nav").innerWidth();
